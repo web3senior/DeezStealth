8331722795fdde19c2f471d4e3131955c3f6263e
Boris Shevchenko <bshevchenkoweb@gmail.com>
2023-11-05 16:42:44 +0300

    pub key form on top

--- src/routes/receiver.jsx
+++ src/routes/receiver.jsx
@@ -61,12 +61,6 @@ export default function Receiver({ title }) {
       <div className={`__container ms-motion-slideUpIn`} data-width={`large`}>
         <div className={`card ms-depth-4 text-justify`}>
           <div className='card__body'>
-            <h3>Generate Stealth Private Key</h3>
-            <p><input type="text" placeholder="Shared Secret" value={sharedSecret} onChange={e => setSharedSecret(e.target.value)} /></p>
-            <p><input type="password" placeholder="Private Key" value={privateKey} onChange={e => setPrivateKey(e.target.value)} /></p>
-            <p><button onClick={handleGenerate}>Generate</button></p>
-            <p>&nbsp;</p>
-
             {!isReady ? (
               <Fragment>
                 <h3>Public Key</h3>
@@ -91,6 +85,12 @@ export default function Receiver({ title }) {
                 )}
               </Fragment>
             )}
+
+            <p>&nbsp;</p>
+            <h3>Generate Stealth Private Key</h3>
+            <p><input type="text" placeholder="Shared Secret" value={sharedSecret} onChange={e => setSharedSecret(e.target.value)} /></p>
+            <p><input type="password" placeholder="Private Key" value={privateKey} onChange={e => setPrivateKey(e.target.value)} /></p>
+            <p><button onClick={handleGenerate}>Generate</button></p>
           </div>
         </div>
       </div>

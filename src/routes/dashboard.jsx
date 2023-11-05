f9abb543d42f6a14183221a11fe8967ca18df7ca
Amir Rahimi <web3senior@gmail.com>
2023-11-05 04:24:22 +0330

    Add chainId

--- src/routes/dashboard.jsx
+++ src/routes/dashboard.jsx
@@ -46,19 +46,7 @@ export default function Dashboard({ title }) {
           {(data) => (
             <ul className={`${styles.food} grid grid--fill`} style={{ '--data-width': '150px' }}>
               {data.map((item, i) => (
-                <li className={`${styles.foodItem} card item${item.id} food-item--active`} key={i} onClick={(e) => handleAddToCart(item.id)}>
-                  <div className={`${styles.foodItemBody} card__body`}>
-                    <figure className={styles.foodItemImage}>
-                      <img alt={item.name} src={`${import.meta.env.VITE_UPLOAD_URL}images/${item.img || 'logo-watermark.png'}`} />
-                    </figure>
-                    <h6 className={styles.foodItemName}>{item.name}</h6>
-                    <small className={styles.foodItemDescription}>{item.description}</small>
-                    <div className={`${styles.foodItemPrice} d-flex align-items-center justify-content-center`} style={{ columnGap: '.3rem' }}>
-                      <span>{new Intl.NumberFormat('fa-IR', { maximumSignificantDigits: 3 }).format(item.price)}</span>
-                      <span className="text text-danger ff-dastnevis">تومان</span>
-                    </div>
-                  </div>
-                </li>
+             
               ))}
             </ul>
           )}
